e344e70f3f19c3d33386414d490c63f92e00ac6e
Miranda Keenan <Miranda Keenan>
2017-01-13 19:33:12 -0500

    trying different path

--- docs/Kevin_Craig_Resume.php
+++ docs/Kevin_Craig_Resume.php
@@ -1,5 +1,5 @@
 <?php
 header("Content-type: application/pdf");
 header("Content-disposition: attachment; filename=Kevin_Craig_Resume.pdf");
-readfile("https://github.com/kevinmmcraig/kevinmmcraig.github.io/docs/Kevin_Craig_Resume.pdf");
+readfile("./docs/Kevin_Craig_Resume.pdf");
 ?>
