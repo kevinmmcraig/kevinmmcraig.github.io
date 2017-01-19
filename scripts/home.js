1bf79662003fe0c53ed7f31be9210e707be2f1ff
kevinmmcraig <kevincraigonline11@gmail.com>
2017-01-18 21:15:19 -0500

    changed images (no alcohol) fixed up layout of pages

--- scripts/home.js
+++ scripts/home.js
@@ -1,12 +1,12 @@
 
 //declarations of them global variables
-var numberofsel = 10;
+var numberofsel = 3;
 var size = 400;
 var quarter = size/4;
 var half = size/2;
 var threequart = 3*size/4;
 
-var sel_txt_array = ["About", "Resume", "Contact", "Just", "To", "Look", "Pretty", ":P", ". . .", "Google!"];
+var sel_txt_array = ["Resume", "Contact", "About", "Just", "To", "Look", "Pretty", ":P", ". . .", "Google!"];
 
 var sel_link_array = ["about.html", "resume.html", "contact.html", "http://www.google.com"];
 var clock_txt_array = ["12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
