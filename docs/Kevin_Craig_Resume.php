2e67dbb4dd6f1351dd716c9f92859594d78b1812
kevinmmcraig <kevincraigonline11@gmail.com>
2017-01-23 19:11:15 -0500

    tiny changes

--- docs/Kevin_Craig_Resume.php
+++
@@ -1,7 +0,0 @@
-<?php
-$filepath = "https://github.com/kevinmmcraig/kevinmmcraig.github.io/docs/Kevin_Craig_Resume.pdf";
-$filename = "Kevin_Craig_Resume.pdf";
-header('Content-Type: application/pdf');
-header('Content-Length: ' . filesize($filepath));
-header('Content-Disposition: attachment; filename=' . $filename);
-readfile($filepath);
